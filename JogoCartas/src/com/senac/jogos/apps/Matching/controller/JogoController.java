8953830714b6ca4b971674d0ad80ea90719966ca
anniemello <annie.mello@gmail.com>
2014-03-27 22:09:04 -0300

    Trabalho_Jogo_Cartas_update

---
+++ JogoCartas/src/com/senac/jogos/apps/Matching/controller/JogoController.java
@@ -0,0 +1,99 @@
+package com.senac.jogos.apps.Matching.controller;
+
+import com.senac.jogos.apps.Matching.model.MatchingCardGame;
+import com.senac.jogos.apps.Matching.view.JogoView;
+import com.senac.jogos.cartas.Carta;
+
+
+public class JogoController {
+
+    private MatchingCardGame jogo;
+    private Carta cartaJogador;
+    private JogoView view;
+    
+    private int posicaoJogador = 0;
+	
+    public JogoController()	{
+        jogo = new MatchingCardGame();
+        view = new JogoView();
+        cartaJogador = null;
+    }
+    
+    public void setPosicaoJogador(int posicao) {
+        if(posicao >= jogo.getNumeroJogadores() || posicao < 0) {
+            posicaoJogador = 0;
+        } else {
+            posicaoJogador = posicao;
+        }
+    }
+    
+    public void iniciarJogo() {
+        view.imprimirCartaMesa(showMesa());
+    }
+
+    public boolean verificarSeTemCarta() {
+        return jogo.verificarSeTemCarta();
+    }
+
+    public void jogada() {
+        view.imprimirSelecionarComando(posicaoJogador+1);
+        String comando = view.lerComando();
+        if(executarComandoJogador(comando)) {
+            if(cartaJogador != null) {
+                view.imprimirCartaJogador(showCarta(cartaJogador));
+            }
+            view.imprimirPontosJogador(showJogador());
+            setPosicaoJogador(posicaoJogador+1);
+        } else {
+            view.imprimirMensagemComandoRepetido();
+        }
+    }
+
+    public void pularVezJogador() {
+        jogo.getJogador(posicaoJogador).subtractPontos(2);
+    }
+
+    public void playTurno() {
+        int pontos = jogo.matchCarta(cartaJogador);
+        jogo.getJogador(posicaoJogador).addPontos(pontos);
+        jogo.setMesa(cartaJogador);
+    }
+    
+    public String showJogador() {
+        return String.format("%d", jogo.getJogador(posicaoJogador).getPontos());
+    }
+	
+    private String showCarta(Carta carta) {
+        return String.format("%d%c", carta.getValor(), carta.getNaipe());
+    }
+
+    public String showMesa() {
+        return showCarta(jogo.getMesa());
+    }
+	
+    public void setCartaJogador() {
+        cartaJogador = jogo.drawCarta();
+        playTurno();
+    }
+
+    public boolean executarComandoJogador(String comando) {
+        comando = comando.equalsIgnoreCase("JOGAR") ? "JOGAR" : "PULAR";
+        
+        
+        if(comando.equals("PULAR") && jogo.getJogador(posicaoJogador).getUltimoComando().equals("PULAR")) {
+            return false;
+        } else if(comando.equalsIgnoreCase("JOGAR")) {
+            this.setCartaJogador();
+        } else {
+            this.pularVezJogador();
+        }
+        jogo.getJogador(posicaoJogador).setUltimoComando(comando);
+        return true;
+    }
+    
+    public void decretarGanhador() {
+        int[] ganhador = jogo.getGanhador();
+        view.imprimirGanhador((ganhador[0]+1), ganhador[1]);
+    }
+            
+}
---
+++ JogoCartas/src/com/senac/jogos/apps/Matching/view/JogoView.java
@@ -0,0 +1,38 @@
+package com.senac.jogos.apps.Matching.view;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+
+
+public class JogoView {
+    private static Scanner teclado = new Scanner(System.in);
+    
+    public String lerComando() {
+        return teclado.nextLine();
+    }
+    
+    public void imprimirCartaMesa(String cartaMesa) {
+        out.println("Carta na mesa: " + cartaMesa);
+    }
+
+    public void imprimirSelecionarComando(int posicao) {
+        //out.print(String.format("Jogador %d. Digite 'jogar' para jogar, 'pular' para passar a vez: ", posicao));
+        out.print(String.format("Jogador %d. Digite 'jogar' ou 'pular': ", posicao));
+    }
+
+    public void imprimirCartaJogador(String cartaJogador) {
+        out.println("Carta do jogador: " + cartaJogador);
+    }
+
+    public void imprimirPontosJogador(String pontosJogador) {
+        out.println("Pontos do Jogador: " + pontosJogador);
+    }
+    
+    public void imprimirMensagemComandoRepetido() {
+        out.println("Voce não pode pular");
+    }
+    
+    public void imprimirGanhador(int posicao, int pontos) {
+        out.println(String.format("O ganhador foi o jogador %d com %d ponto(s)", posicao, pontos));
+    }
+}
